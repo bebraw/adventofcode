495f204c840a8d19a235257eaa72d9e6f7789ea3
Juho Vepsalainen <bebraw@gmail.com>
2018-12-09 13:50:41 +0100

    chore: Refine TS integration

--- src/02-02.ts
+++ src/02-02.ts
@@ -1,37 +1,44 @@
 import * as fs from "fs";
 
+type Word = {
+  distance: number;
+  word?: string;
+  match?: string;
+};
+
 //const testWords = 'abcde,fghij,klmno,pqrst,fguij,axcye,wvxyz'.split(',').map(a => a.split(''));
-const words = fs
+const words: string[][] = fs
   .readFileSync("./input/02.txt", "utf8")
   .split("\n")
   .map(a => a.split(""));
 
 let shortest = { distance: words[0].length };
-const distances = words.map((word, i) => {
-  if (i === words.length - 1) {
-    return;
-  }
+const distances = words.slice(0, -1).map(
+  word =>
+    words
+      .map(w =>
+        word !== w
+          ? {
+              distance: distanceTo(word, w),
+              word: word.filter((l, i) => l === w[i]).join(""),
+              match: w.join("")
+            }
+          : {
+              distance: word.length,
+              word: w
+            }
+      )
+      .sort((a, b) => a.distance - b.distance)[0]
+);
 
-  const distanceToWord = distanceTo(word);
-  return words
-    .map(w =>
-      word !== w
-        ? {
-            distance: distanceToWord(w),
-            word: word.filter((l, i) => l === w[i]).join(""),
-            match: w.join("")
-          }
-        : {
-            distance: word.length,
-            word: w
-          }
-    )
-    .sort((a, b) => a.distance - b.distance)[0];
-});
-
-console.log(shortest, distances.sort((a, b) => a.distance - b.distance)[0]);
+console.log(
+  shortest,
+  distances
+    .map(a => a.distance)
+    .sort((a: Word["distance"], b: Word["distance"]) => a - b)[0]
+);
 
 // Assumes strings have the same length
-function distanceTo(a) {
-  return b => a.filter((n, i) => n !== b[i]).length;
+function distanceTo(a: string[], b: string[]): number {
+  return a.filter((n, i) => n !== b[i]).length;
 }
